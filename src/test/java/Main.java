53830c4dc2596b710606bd98519a0b89478b94fa
Kirtan Soni <kgsoni@asu.edu>
2023-10-28 18:30:57 -0700

    Resolving Merge Conflicts

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-         // to run tests, make sure Assertion is enabled in your IDE or run with -ea flag.
-       UserStoryTests userStoryTests = new UserStoryTests();
-       ProductBacklogTests productBacklogTests = new ProductBacklogTests();
+
+        // to run tests, make sure Assertion is enabled in your IDE or run with -ea flag.
+        UserStoryTests userStoryTests = new UserStoryTests();
+        ProductBacklogTests productBacklogTests = new ProductBacklogTests();
+        
 
     }
 
