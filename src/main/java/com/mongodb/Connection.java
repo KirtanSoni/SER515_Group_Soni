f50887bf52d1f954561a15b22bcb676a54ca4911
Shubham Shah <sshah232@asu.edu>
2023-10-30 23:02:05 -0700

    CRUD Operations updated. Passed all the test cases

--- src/main/java/com/mongodb/Connection.java
+++ src/main/java/com/mongodb/Connection.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Connection {
 
     public static void main(String[] args) {
-        String connectionString = System.getProperty("mongodb.uri");
+        String connectionString = System.getProperty("mongodb+srv://sshah232:ye6yVTzEYA3WdBVj@scrumsimulator.nuu1fks.mongodb.net/");
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
             List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
             databases.forEach(db -> System.out.println(db.toJson()));
