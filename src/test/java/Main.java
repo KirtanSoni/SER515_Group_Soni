65b778388629ff30be85a51ea966d240e6212ea3
mpraja11 <mpraja11@asu.edu>
2023-11-03 12:19:58 -0700

    DBoperations files deleted from everywhere in US#62-new because of errors.

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -11,7 +11,6 @@ public class Main {
          HomePageTests homePageTests = new HomePageTests();
          SprintBacklogTests sprintBacklogTests = new SprintBacklogTests();
          HelperTest helpersTest = new HelperTest();
-//        DBTest.main(args);
          AddUserStoryTests addUserStoryTests = new AddUserStoryTests();
 
 
