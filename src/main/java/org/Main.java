e5950145b9a344452c57d04a2d5be1761b36bc3f
JANMAY007 <jrbhatt18p10@gmail.com>
2023-11-09 20:14:21 -0700

    Solving Conflicts

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,5 +1,8 @@
+package org;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         // Create a UserManagement instance
         UserManagement userManagement = new UserManagement();
 
