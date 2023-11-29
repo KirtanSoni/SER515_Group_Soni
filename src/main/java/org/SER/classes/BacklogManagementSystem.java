6c6b1a29e619bb3f8061351cbb62b22be0f2b407
JANMAY007 <jrbhatt18p10@gmail.com>
2023-11-29 16:00:03 -0700

    Updated backlog management with necessary comments

--- src/main/java/org/SER/classes/BacklogManagementSystem.java
+++ src/main/java/org/SER/classes/BacklogManagementSystem.java
@@ -10,7 +10,7 @@ public class BacklogManagementSystem {
         this.productBacklog = productBacklog;
     }
 
-    public void manageBacklog(User user) {
+    public void manageBacklog(User user, UserStory userStory) {
         UserType userType = user.getUserType();
 
         if (userType == UserType.DEVELOPER) {
@@ -19,27 +19,28 @@ public class BacklogManagementSystem {
             System.out.println("Sprint Backlog: " + sprintBacklog.getsprintBacklog());
 
             // Example: Update status of a User Story in Sprint Backlog
-            int userStoryIdToUpdate = 1; // Replace with the actual User Story ID
+            int userStoryIdToUpdate = userStory.getId();
             Status newStatus = Status.IN_PROGRESS;
             sprintBacklog.updateStatus(userStoryIdToUpdate, newStatus);
 
             // Example: Remove a User Story from Sprint Backlog
-            int userStoryIdToRemove = 2; // Replace with the actual User Story ID
+            int userStoryIdToRemove = userStory.getId();
             sprintBacklog.removeUserStory(userStoryIdToRemove);
 
             // You can add more functionality specific to developers here
         } else if (userType == UserType.PRODUCT_OWNER) {
             // Product Owner can view and update Product Backlog
             System.out.println("Product Owner Access:");
-            System.out.println("Product Backlog: " + productBacklog.getProductBacklog());
+            // System.out.println("Product Backlog: " + productBacklog.getProductBacklog());
 
             // Example: Update priority of a User Story in Product Backlog
-            int userStoryIdToUpdate = 1; // Replace with the actual User Story ID
-            int newPriority = 2; // Replace with the new priority
-            productBacklog.updatePriority(userStoryIdToUpdate, newPriority);
+            // add priority params if required
+            // int userStoryIdToUpdate = userStory.getId();
+            // int newPriority = priority;
+            // productBacklog.updatePriority(userStoryIdToUpdate, newPriority);
 
             // Example: Remove a User Story from Product Backlog
-            int userStoryIdToRemove = 3; // Replace with the actual User Story ID
+            int userStoryIdToRemove = userStory.getId(); // Replace with the actual User Story ID
             productBacklog.removeUserStory(userStoryIdToRemove);
 
             // You can add more functionality specific to product owners here
